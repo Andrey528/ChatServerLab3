fa4727d72bf2fcd129d71890a41a5abf7ff8dd92
Andrew <telepnev.1997@mail.ru>
2024-03-19 22:34:24 +0700

    Добавьте файлы проекта.

---
+++ ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,212 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _IP;
+        public string IP { get => _IP; set => _IP = value; }
+
+        private int _Port;
+        public int Port { get => _Port; set => _Port = value; }
+
+        private string _Nick;
+        public string Nick { get => _Nick; set => _Nick = value; }
+
+        private string _Chat;
+        public string Chat { get => _Chat; set => _Chat = value; }
+
+        private string _Message;
+        public string Message { get => _Message; set => _Message = value; }
+
+        private CancellationTokenSource _cancellationTokenSource;
+
+        private ObservableCollection<string> _ConnectedClients = new ObservableCollection<string>();
+        public ObservableCollection<string> ConnectedClients { get => _ConnectedClients; set => _ConnectedClients = value; }
+
+        private TcpClient client;
+        private StreamWriter sw;
+        private StreamReader sr;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IP = "192.168.0.10";
+            Port = 5050;
+            Nick = "Jack";
+
+            txtIp.Text = IP;
+            txtPort.Text = Port.ToString();
+            txtNick.Text = Nick;
+        }
+
+        public void ConnectCommand(object sender, RoutedEventArgs e)
+        {
+            IP = txtIp.Text;
+            Port = Convert.ToInt16(txtPort.Text);
+            Nick = txtNick.Text;
+
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect(IP, Port);
+                sr = new StreamReader(client.GetStream()); // Буффер для чтения сообщений
+                sw = new StreamWriter(client.GetStream()); // Буффер для написания сообщений
+
+                sw.AutoFlush = true; // Окончание работы с буфферами
+
+                sw.WriteLine($"Login: {Nick}"); // Сразу как подключился, представляемся серверу
+
+                Thread listenThread = new Thread(() => ReadMessages(_cancellationTokenSource.Token));
+                listenThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void ReadMessages(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var line = sr.ReadLine();
+                    // если пустой ответ, ничего не выводим на консоль
+                    if (string.IsNullOrEmpty(line)) continue;
+
+                    if (line.StartsWith("ConnectedClients: "))
+                    {
+                        string[] connectedClients = line.Replace("ConnectedClients: ", "").Split(',');
+                        ObservableCollection<string> updatedConnectedClients = new ObservableCollection<string>();
+
+                        foreach (var client in connectedClients)
+                        {
+                            updatedConnectedClients.Add(client);
+                        }
+
+                        Dispatcher.Invoke(() =>
+                        {
+                            ConnectedClients.Clear(); // Очищаем текущий список подключенных клиентов
+
+                            foreach (var client in updatedConnectedClients)
+                            {
+                                ConnectedClients.Add(client);
+                            }
+
+                            clientsDataGrid.ItemsSource = ConnectedClients;
+                        });
+                    }
+                    else
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            txtChat.Text += line + "\n";
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show(ex.Message);
+                });
+            }
+            finally
+            {
+                try
+                {
+                    if (client != null && client.Connected)
+                    {
+                        client.GetStream().Close();
+                        client.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public void SendMessage(object sender, RoutedEventArgs e) // Метод асинхронной отправки сообщения
+        {
+            Message = txtMessage.Text;
+
+            if (client?.Connected == true && !string.IsNullOrWhiteSpace(Message))
+            {
+                var recipient = clientsDataGrid.SelectedItem;
+
+                if (recipient != null)
+                {
+                    try
+                    {
+                        string personalMessage = $"FROM: {Nick}, TO: {recipient}, BODY: {Message}";
+                        sw.WriteLine(personalMessage);
+                        txtMessage.Text = ""; // Очищаем поле ввода сообщения
+                        clientsDataGrid.SelectedItem = null; // Очищаем выбор пользователя из списка подключенных пользователей
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                } 
+                else
+                {
+                    try
+                    {
+                        string message = $"{Nick}: {Message}";
+                        sw.WriteLine(message);
+                        txtMessage.Text = ""; // Очищаем поле ввода сообщения
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            try
+            {
+                _cancellationTokenSource?.Cancel();
+
+                if (client?.Connected == true) // Проверяем, что клиент подключен
+                {
+                    sw.WriteLine("Disconnect"); // Отправляем серверу сообщение, что клиент отключается
+                    client.GetStream().Close(); // Закрываем поток данных клиента
+                    client.Close(); // Закрываем соединение
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ ChatServerLab3/Program.cs
@@ -0,0 +1,152 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using ChatServerLab3;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static TcpListener listener = new TcpListener(IPAddress.Any, 5050);
+    static List<ConnectedClient> clients = new List<ConnectedClient>();
+    static Regex regex = new Regex(@"FROM: (.*?), TO: (.*?), BODY: (.*)");
+
+    static void Main(string[] args)
+    {
+        listener.Start();
+
+        while (true)
+        {
+            var client = listener.AcceptTcpClient();
+
+            Task.Factory.StartNew(async () =>
+            {
+                var sr = new StreamReader(client.GetStream());
+
+                while (client.Connected) // Если клиент подключен
+                {
+                    var line = sr.ReadLine(); // Читаем отправленную им строку
+
+                    if (line.Contains("Login: ") && !string.IsNullOrWhiteSpace(line.Replace("Login: ", ""))) // Есть ли логин клиента?
+                    {
+                        var nick = line.Replace("Login: ", ""); // Берем ник клиента
+
+                        if (clients.FirstOrDefault(s => s.Name == nick) == null) // Есть ли такой ник среди клиентов?
+                        {
+                            clients.Add(new ConnectedClient(client, nick)); // Если такого клиента нет, то мы его добавляем
+                            
+                            Console.WriteLine($"New connection: {nick}"); // Лог сервера - говорим, что клиент подключен
+                            ShowConnectedClients();
+
+                            await SendConnectedClients();
+
+                            break;
+                        }
+                        else
+                        {
+                            var sw = new StreamWriter(client.GetStream());
+                            sw.AutoFlush = true;
+
+                            sw.WriteLine("Пользователь с таким ником уже есть в чате");
+                            client.Client.Disconnect(false); // Если клиент с таким ником уже есть, то мы его отключаем
+                        }
+                    }
+
+
+                }
+
+                while (client.Connected)
+                {
+                    try
+                    {
+                        sr = new StreamReader(client.GetStream());
+                        var line = sr.ReadLine();
+
+                        if (line == "Disconnect") // Проверяем, если клиент отключился
+                        {
+                            var disconnectedClient = clients.FirstOrDefault(c => c.Client == client);
+                            clients.Remove(disconnectedClient); // Удаляем отключенного клиента из списка
+
+                            ShowConnectedClients();
+
+                            await SendConnectedClients(); // Отправляем обновленный список клиентов всем клиентам
+
+                            // Закрыть соединение и освободить ресурсы
+                            client.GetStream().Close();
+                            client.Close();
+                            client.Dispose();
+                            break;
+                        }
+
+                        Match match = regex.Match(line);
+
+                        if (match.Success) // Регулярное выражение проверяет на шаблон персонального сообщения
+                        {
+                            string sender = match.Groups[1].Value.Trim();
+                            string recipient = match.Groups[2].Value.Trim();
+                            string message = match.Groups[3].Value.Trim();
+
+                            await SendPersonalMessage(sender, recipient, message);
+                        }
+                        else
+                        {
+                            await SendToAllClients(line); // Если клиент подключен и когда прийдет строчка сообщения на сервер, то мы отправим сообщение пользователям
+                        }
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            });
+        }
+
+        static async Task SendPersonalMessage(string sender, string recipient, string message)
+        {
+            var recipientClient = clients.FirstOrDefault(c => c.Name == recipient); // Находим клиента по нику получателя
+
+
+            if (recipientClient != null && recipientClient.Client.Connected) // Если клиент существует и подключен
+            {
+                var sw = new StreamWriter(recipientClient.Client.GetStream());
+                sw.AutoFlush = true;
+                await sw.WriteLineAsync($"Personal message from {sender}: {message}"); // Отправляем сообщение клиенту
+            }
+        }
+
+        static async Task SendToAllClients(string message)
+        {
+            Console.WriteLine(message);
+
+            foreach (var client in clients)
+            {
+                Console.WriteLine(message);
+
+                var sw = new StreamWriter(client.Client.GetStream());
+                await sw.WriteLineAsync(message); //передача данных
+                await sw.FlushAsync();
+            }
+        }
+
+        static async Task SendConnectedClients()
+        {
+            var connectedClients = string.Join(",", clients.Select(c => c.Name)); // Соединяем имена клиентов через запятую
+            var message = $"ConnectedClients: {connectedClients}";
+
+            foreach (var client in clients)
+            {
+                var sw = new StreamWriter(client.Client.GetStream());
+                await sw.WriteLineAsync(message);
+                await sw.FlushAsync(); // Очищаем буфер
+            }
+        }
+
+        static void ShowConnectedClients()
+        {
+            Console.WriteLine("Remaining connected clients: "); // Вывод в консоль на сервер подключенных клиентов
+            foreach (var connectedClient in clients)
+            {
+                Console.WriteLine("- " + connectedClient.Name);
+            }
+        }
+    }
+}
